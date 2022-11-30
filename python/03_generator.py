59979cbdb25b853fe7848eb2ff32515fce8f645b
youruchul <youruchul@naver.com>
2022-11-30 17:55:34 +0900

    22.11.30

---
+++ python/03_generator.py
@@ -0,0 +1,13 @@
+'''
+    (매우 중요)
+    generator(제너레이터)
+        1. 특징
+            - 한 번에 한개씩만 생성되고 메모리에 유지x
+            - next()를 사용해서 하나씩 값을 얻는다.
+            - for문 또는 list()를 통해 값 확인 가능
+
+        2. 활용
+        ==> generator comprehension (     )
+        ==> range()가 대표적인 generator
+        ==> generator 함수 
+'''
